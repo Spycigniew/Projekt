ec2ff79aacf40b9f274264623a98fd8d6b025f92
S22IP104GA06\Uczeń <areknaranowicz@gmail.com>
2018-04-15 15:58:56 +0200

    no message

--- NowyProjekt/Nowy.py
+++ NowyProjekt/Nowy.py
@@ -1,2 +1,22 @@
 print("arek")
 
+
+def dodawanie(x, y):
+    suma= x + y
+    return suma
+
+def odejmowanie(x, y):
+    roznica = x - y
+    return roznica
+
+def mnozenie(x, y):
+    iloczyn = x * y
+    return iloczyn
+
+def dzielenie(x, y):
+    iloraz = x/y
+    return iloraz
+print(dodawanie(4,5))
+print(odejmowanie(9,5))
+print(mnozenie(2,3))
+print(dzielenie(9,3))
--- NowyProjekt/venv/macierz.py
+++ NowyProjekt/venv/macierz.py
@@ -4,3 +4,25 @@ for i in range(3):
     print("\n")
     for j in range(3):
         print(macierz[i][j],end=" ")
+
+
+
+def dodawanie(x, y):
+    suma= x + y
+    return suma
+
+def odejmowanie(x, y):
+    roznica = x - y
+    return roznica
+
+def mnozenie(x, y):
+    iloczyn = x * y
+    return iloczyn
+
+def dzielenie(x, y):
+    iloraz = x/y
+    return iloraz
+
+
+
+print(suma = 5 +4)
