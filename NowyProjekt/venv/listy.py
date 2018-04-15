30949c941f861ca19119d229ebe17eb77eb2ac76
S22IP104GA06\Uczeń <areknaranowicz@gmail.com>
2018-04-15 15:35:16 +0200

    0.1 Initial comment

---
+++ NowyProjekt/venv/listy.py
@@ -0,0 +1,42 @@
+L1= [1,2,3,4]
+L2= ['string1','stringonnnajdjssdsds3','string']
+L3= [3.144440,3.56,3.50,3.145698]
+L4= ['sfffff', 3.14, 8]
+L5= ['d','u','L','A','G','d']
+
+# print(L1)
+# print(L2)
+# print(L3)
+# print(L4)
+# print(L5)
+
+print("Ext")
+L1.extend([5,6,7,8,9])
+L2.extend(['lkajdjksjgow','dk4343443g8','stringsdsd9','string0','string6'])
+L3.extend([4.40,5.60,9,20,3.30,12.50])
+L4.extend([5,23.33,'string10',12,5.55])
+L5.extend(['D','s','T','R','D'])
+print(L1,L2,L3,L4,L5, sep="\n\n\n")
+
+print("djjkjskjdksdsd")
+L1.sort()
+L2.sort()
+L3.sort()
+L5.sort()
+print(L1,L8,L3,L5, sep="\n\n\n")
+
+print("odw")
+L1.reverse()
+L2.reverse()
+L3.reverse()
+L4.reverse()
+L5.reverse()
+print(L1,L2,L3,L5, sep="\n\n\n\n")
+
+print("us")
+L1.pop(2)
+L2.pop(2)
+L3.pop(2)
+L4.pop(2)
+L5.pop(2)
+print(L1,L2,L3,L5, sep="\n")
---
+++ NowyProjekt/venv/macierz.py
@@ -0,0 +1,6 @@
+macierz =[[0,0,1],[1,0,0],[0,1,0]]
+
+for i in range(3):
+    print("\n")
+    for j in range(3):
+        print(macierz[i][j],end=" ")
